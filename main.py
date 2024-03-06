38c424ff977d8b55aadf124f6962403f422539b9
Victor Hugo Malheiro <victorhugomalheiro@gmail.com>
2024-03-06 11:24:41 -0300

    ADD gui

--- main.py
+++ main.py
@@ -1,19 +1,50 @@
 import serial
+import tkinter as tk
+import platform
 
-# Serial port configs
-#porta_serial = '/dev/ttyUSB0'
-serial_port = 'COM5'
-baud_rate = 9600
+def Interface():
+    app = tk.Tk()
+    app.title("Send Message")
+    app.geometry("200x200")
 
-# Receive and send the message to arduino
+    msg_label = tk.Label(app, text="Digite o comando (a): ")
+    msg_label.pack()
+
+    global msg_entry
+    msg_entry = tk.Entry(app)
+    msg_entry.insert(tk.END, "a")
+    msg_entry.pack()
+
+    port_label = tk.Label(app, text="Serial Port:")
+    port_label.pack()
+
+    global serial_port
+    serial_port = tk.Entry(app)
+    serial_port.insert(tk.END, "COM5" if platform.system() == "Windows" else "/dev/ttyUSB0")
+    serial_port.pack()
+
+    baud_label = tk.Label(app, text="Baud Rate:")
+    baud_label.pack()
+
+    global baud_rate
+    baud_rate = tk.Entry(app)
+    baud_rate.insert(tk.END, "9600")
+    baud_rate.pack()
+
+    send_button = tk.Button(app, text="Enviar Mensagem", command=SendoToArduino)
+    send_button.pack()
+
+    app.mainloop()
+
+# Receive the message and try connection
 def SendoToArduino():
-    msg = input("Digite o comando (a): ")
+    msg = msg_entry.get()
     TryConnection(msg)
 
 # Try connection and send message
 def TryConnection(message):
     try:
-        ser = serial.Serial(serial_port, baud_rate, timeout=1)
+        ser = serial.Serial(serial_port.get(), baud_rate.get(), timeout=1)
         print("Connection Established")
         ser.write(message.encode())
         ser.close()
@@ -21,4 +52,4 @@ def TryConnection(message):
         print("Connection Error. Check the Port")
 
 if __name__ == "__main__":
-    SendoToArduino()    
+    Interface()    
